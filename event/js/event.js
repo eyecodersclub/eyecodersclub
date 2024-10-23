c5abf42593d948b96e9c39fb47c35bf05c3bb809
soni-shashan <lumbhanishashan1510@gmail.com>
2024-10-23 12:30:47 +0530

    update event api

--- event/js/event.js
+++ event/js/event.js
@@ -1,6 +1,16 @@
 import { fetchEvents } from '../../src/api/event.js';
 
-
+function formatDateTime(dateStr) {
+    const date = new Date(dateStr);
+    return date.toLocaleString('en-US', { 
+        year: 'numeric', 
+        month: 'long', 
+        day: 'numeric', 
+        hour: '2-digit', 
+        minute: '2-digit', 
+        hour12: false 
+    });
+}
 
 const eventId = new URLSearchParams(window.location.search).get('eventId');
 if  (eventId) {
@@ -12,6 +22,8 @@ if  (eventId) {
                 document.getElementById("event_title").innerHTML=event["\"Title\""];
                 document.getElementById("event_description").innerHTML=event["\"Description\""];
                 document.getElementById("event_image").src=`https://drive.google.com/thumbnail?id=${event["\"Image ID\""]}&sz=w1000`;
+                document.getElementById("event_date").textContent = `From: ${formatDateTime(event["\"Start Date\""])} To: ${formatDateTime(event["\"End Date\""])}`;
+    
                 const loadingScreen = document.getElementById('loading-screen');
                 const content = document.getElementById('content');
                 

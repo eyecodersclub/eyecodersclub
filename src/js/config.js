9a86749c39a21321fd7d4f8e9e32be5a4a677ba3
soni-shashan <lumbhanishashan1510@gmail.com>
2024-10-25 08:51:19 +0530

    add api

---
+++ src/js/config.js
@@ -0,0 +1,14 @@
+// config.js
+export const joinClub = {
+    BASE_URL: "https://script.google.com/macros/s",
+    API_KEY: "AKfycbzavrThvrlSm-WSXMhLOM_7ncbYXKKYBm3_tS4pvoYN9DJK5QywsK2JZNhvU3ZqDR_s",
+  };
+export const team = {
+    BASE_URL: "https://script.google.com/macros/s",
+    API_KEY: "AKfycbxfpoCsVH6lGCXRIRDvvtAteL367yMLeQDzfyfADD9uFNREVFmiskhebD4KcFAS9TtsbQ",
+  };
+export const event = {
+    BASE_URL: "https://script.google.com/macros/s",
+    API_KEY: "AKfycbwhacINqTptjo9YOSQWcY-sv31wn_d_7L-Qz_uxywex673xw3lG3RyF_A15t_AGPR-5iw",
+  };
+  

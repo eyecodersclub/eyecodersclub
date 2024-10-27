1eaee2e6987b5819aeb12a780b62f11604f450aa
soni-shashan <lumbhanishashan1510@gmail.com>
2024-10-27 14:32:10 +0530

    Update config.js

--- src/js/config.js
+++ src/js/config.js
@@ -1,2 +1,2 @@
 export const BASE_URL = 'https://script.google.com/macros/s';
-export const API_KEY = 'AKfycbxmZPFZbKywQgZErYRwnWwT6zcgbN7jjvfqreoe0Z5-3p9JMSEdyeD5FHCJWKUhEhwtHw';
+export const API_KEY = 'AKfycbwRM_cjFkB1x3J2CSg5cabvboFiF2rp2tAdPyHX7ljpKq8SNpZqVu3s9xSLlbzb6K_Q';
